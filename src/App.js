7c4d186dfedb216bfafae7f9d08220e97ce4eeab
Theo <sagniez.theo@laposte.net>
2023-11-20 13:46:23 +0100

    created slide + appear animations when clicking on Next button

--- src/App.js
+++ src/App.js
@@ -4,25 +4,24 @@ import BasicForm from './components/BasicForm';
 import { questions } from './content/questions'
 
 function App() {
-  const [currQuestion, setCurrQuestion] = useState(0)
+  // const [currQuestion, setCurrQuestion] = useState(0)
   const [animate, setAnimate] = useState(false)
 
   return (
-    <div className="App">
-      {
-        currQuestion < questions.length ? 
-          <BasicForm question={questions[currQuestion]}
-                     animate={animate} 
-          />
-        :
-        <span>Thank you for your answers</span>
-      }
+    <div className="App"> 
+      <BasicForm question={questions[0]}
+                  animate={animate} 
+      />
       
 
       <button onClick={() => {
-        setCurrQuestion(currQuestion + 1) 
+        // setCurrQuestion(currQuestion + 1) 
         setAnimate(true)
       }}>Next</button>
+      <button onClick={() => {
+        // setCurrQuestion(currQuestion + 1) 
+        setAnimate(false)
+      }}>Reset</button>
     </div>
   );
 }
